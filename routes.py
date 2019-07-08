7c8a002004a48ddc609873f2c704862cd1a47269
MUTHIA Elifia <44280@dhcp-10-250-17-116.harvard.edu>
2019-07-07 23:37:35 -0400

    Deploying to Heroku

--- routes.py
+++ routes.py
@@ -5,10 +5,10 @@ app = Flask(__name__) #instantiate the Flask class
 
 @app.route("/") #if the path is empty
 def index():
+	model = joblib.load('regr.pkl')
 	prediction = model.predict([[4, 2.5, 3005, 15, 17903.0]]).round(1)
 	prediction = str(prediction)
 	return render_template("index.html", prediction=prediction)
 
 if __name__ == "__main__":
-	model = joblib.load('regr.pkl')
 	app.run(debug=True)
